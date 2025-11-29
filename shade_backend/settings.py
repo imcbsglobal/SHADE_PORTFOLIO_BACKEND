deeaab623b5a37d2ad67de41f93ebb30ead951a5
Rashmitha nr <rashmithanrrashmithanr@gmail.com>
2025-11-29 10:22:08 +0530

    api chnage

--- shade_backend/settings.py
+++ shade_backend/settings.py
@@ -153,13 +153,13 @@ CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5174",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:5174",
-    "https://shade.imcbs.com/"
+    "https://shade.imcbs.com"  
 ]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:5174",
-    "https://shade.imcbs.com/",
+    "https://shade.imcbs.com",  
 ]
 
 # ==========================================
